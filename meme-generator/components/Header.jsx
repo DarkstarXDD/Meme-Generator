6b6852b60ddbde5716636aaf60f8218c074407ff
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2023-11-28 22:49:15 +0530

    Created Header

---
+++ meme-generator/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <div className="header">
+            <img className="troll-face-img" src="./assets/troll-face.png"></img>
+            <h1 className="header-left-title">Meme Generator</h1>
+            <p className="header-right-title">React Course - Project 3</p>
+        </div>
+    )
+}
