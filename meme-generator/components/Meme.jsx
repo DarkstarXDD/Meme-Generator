7d5fe6c63345c00ec9d8df976c7d92631758093a
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2023-11-29 17:58:17 +0530

    Added an onClick Event Handler to the Button, to return a random URL from the memesData.js

--- meme-generator/components/Meme.jsx
+++ meme-generator/components/Meme.jsx
@@ -1,13 +1,24 @@
+import memesData from "../memesData"
+
 export default function Meme() {
+
+    function handleButtonClick() {
+        const randNumber = Math.floor((Math.random() * 100))
+        console.log(randNumber)
+        
+        console.log(memesData.data.memes[randNumber].url)
+    }
+
     return (
         <div className="meme-section">
-            <form>
-                <div className="two-inputs">
-                    <input className="input-txt" type="text" placeholder="Top Text" />
-                    <input className="input-txt" type="text" placeholder="Bottom Text" />
-                </div>
-                <input className="btn-submit" type="submit" value="Get a New Meme Image"/>
-            </form>
+
+            <div className="two-inputs">
+                <input className="input-txt" type="text" placeholder="Top Text" />
+                <input className="input-txt" type="text" placeholder="Bottom Text" />
+            </div>
+
+            <input onClick={handleButtonClick} className="btn-submit" type="submit" value="Get a New Meme Image"/>
+
         </div>
     )
 }
