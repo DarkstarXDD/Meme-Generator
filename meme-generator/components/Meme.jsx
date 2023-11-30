d349b1503c87305b43baaa3f4285caeb22c5a0f7
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2023-12-01 01:30:21 +0530

    Added a React State to get a random image and update the page

--- meme-generator/components/Meme.jsx
+++ meme-generator/components/Meme.jsx
@@ -1,12 +1,17 @@
+import React from "react"
 import memesData from "../memesData"
 
 export default function Meme() {
 
-    function handleButtonClick() {
-        const randNumber = Math.floor((Math.random() * 100))
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getRandomMeme() {
+        const randNumber = Math.floor((Math.random() * memesData.data.memes.length))
         console.log(randNumber)
-        
-        console.log(memesData.data.memes[randNumber].url)
+
+        const url = memesData.data.memes[randNumber].url
+        console.log(url)
+        setMemeImage(url)
     }
 
     return (
@@ -17,7 +22,9 @@ export default function Meme() {
                 <input className="input-txt" type="text" placeholder="Bottom Text" />
             </div>
 
-            <input onClick={handleButtonClick} className="btn-submit" type="submit" value="Get a New Meme Image"/>
+            <input onClick={getRandomMeme} className="btn-submit" type="submit" value="Get a New Meme Image"/>
+
+            <img className="meme-img" src={memeImage} />
 
         </div>
     )
